edec4a9b38e9739d30ede9d9cdcbf03c8bbbfee5
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-13 18:36:34 +0530

    Add files via upload

---
+++ Techstuff_training/Day01.js
@@ -0,0 +1,21 @@
+/* 1.  Create a js file which contains variable declaration for a form which asks following basic details :
+(What is your Name, Email, age, address, phone number, gender,
+ Are you married) */
+var name = ("What is your name ?")
+var Email = ("Enter your Email ?")
+var age =  ("What is your age ?")
+var  address = ("What is your address ?")
+var phone_number = ("What is your phone number ?")
+var  gender = ("What is your gender ?") 
+var  married = ("Are you married ?")
+function printName() {
+    console.log(name)
+    console.log(Email)
+    console.log(age)
+    console.log(address)
+    console.log(phone_number)
+    console.log(gender)
+    console.log(married)
+}
+printName()
+
