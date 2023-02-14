2059d4638e4da8f63bb3e508ffca8755f92ce1f8
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-14 18:37:13 +0530

    Add files via upload

---
+++ Techstuff_training/Day03_DataType.js
@@ -0,0 +1,17 @@
+// 3. Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object.
+const employeeDetails = {
+    employeeName : "Name",
+    employeeEmail : "@gmail.com",
+    employeeAge : 0,
+    employeeAddress : "Ujjain (M.P.)",
+    employeePhoneNumber : "+91 0000 0000 00",
+    employeeGender : " ",
+    employeeisMarried : false,
+}
+console.log(employeeDetails["employeeName"]),
+console.log(employeeDetails["employeeEmail"]),
+console.log(employeeDetails["employeeAge"]),
+console.log(employeeDetails["employeeAddress"]),
+console.log(employeeDetails["employeePhoneNumber"]),
+console.log(employeeDetails["employeeGender"]),
+console.log(employeeDetails["employeeisMarried"])
