dad2674d9c1afc93cccdf322235c7b3957309ab0
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-14 11:22:03 +0530

    Update Day01.js

--- Techstuff_training/Day01.js
+++ Techstuff_training/Day01.js
@@ -1,20 +1,20 @@
 /* 1.  Create a js file which contains variable declaration for a form which asks following basic details :
 (What is your Name, Email, age, address, phone number, gender,
  Are you married) */
-Name= ("What is your name ?")
-Email = ("Enter your Email ?")
-Age =  ("What is your age ?")
-Address = ("What is your address ?")
-Phone_number = ("What is your phone number ?")
-Gender = ("What is your gender ?") 
-Married = ("Are you married ?")
-function printName() {
-    console.log(Name)
-    console.log(Email)
-    console.log(Age)
-    console.log(Address)
-    console.log(Phone_number)
-    console.log(Gender)
-    console.log(Married)
+let employeeeName = ("What is your name ?")
+let employeeEmail = ("Enter your Email Address.")
+let employeeAge =  ("What is your age ?")
+let employeeAddress = ("What is your address ?")
+let employeePhoneNumber = ("What is your phone number ?")
+let employeeGender = ("What is your gender ?") 
+let employeeMarried = ("Are you married ?")
+function printDetails() {
+    console.log(employeeeName)
+    console.log(employeeEmail)
+    console.log(employeeAge)
+    console.log(employeeAddress)
+    console.log(employeePhoneNumber)
+    console.log(employeeGender)
+    console.log(employeeMarried)
 }
-printName()
+printDetails()
