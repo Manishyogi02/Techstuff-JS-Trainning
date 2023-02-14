3faaed348cd896530429e56cc7b137ddec3010e3
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-14 11:42:33 +0530

    Update Day02_Object.js

--- Techstuff_training/Day02_Object.js
+++ Techstuff_training/Day02_Object.js
@@ -1,17 +1,17 @@
 // 3. Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object.
 const employeeDetails = {
-    "Name" : null,
-    "Email" : null,
-    "Age" : null,
-    "Address" : null,
-    "PhoneNumber" : null,
-    "Gender" : null,
-    "Maritial" : null,
+    "employeeName" : null,
+    "employeeEmail" : null,
+    "employeeAge" : null,
+    "employeeAddress" : null,
+    "employeePhoneNumber" : null,
+    "employeeGender" : null,
+    "employeeMaritialStatus" : null,
 }
-console.log(employeeDetails["Name"]),
-console.log(employeeDetails["Email"]),
-console.log(employeeDetails["Age"]),
-console.log(employeeDetails["Address"]),
-console.log(employeeDetails["PhoneNumber"]),
-console.log(employeeDetails["Gender"]),
-console.log(employeeDetails["Maritial"])
+console.log(employeeDetails["employeeName"]),
+console.log(employeeDetails["employeeEmail"]),
+console.log(employeeDetails["employeeAge"]),
+console.log(employeeDetails["employeeAddress"]),
+console.log(employeeDetails["employeePhoneNumber"]),
+console.log(employeeDetails["employeeGender"]),
+console.log(employeeDetails["employeeMaritialStatus"])
