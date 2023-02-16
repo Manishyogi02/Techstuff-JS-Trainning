65c065a880052b736c69a2804ed4fbc1fc3561fc
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-16 15:44:01 +0530

    Add files via upload

---
+++ Techstuff_training/Day05_loop.js
@@ -0,0 +1,33 @@
+//1. Find sum of first 10 natural numbers.
+let sum = 0
+//let n = prompt("Enter any number :-")
+console.log("Sum of first 10 natural numbers is :- ")
+for (let i = 1; i < 11; i++) {
+  sum += i+1
+}
+console.log(sum)
+//console.log("sum of first " + n + " natural number is  " + sum)
+
+// 2. Print fibbonaci series upto first 10 numbers.
+console.log("Fabinocci Series of 10 number")
+let n1 = 0, n2 = 1, nextTerm
+for(let i = 0; i < 10; i++){
+    console.log(n1)
+    nextTerm = n1 + n2
+    n1 = n2
+    n2 = nextTerm
+}
+
+// 
+let obj ={
+    employeeName: "Manish Yogi",
+    employeeEmail: "manishnathyogi333@gmail.com",
+    employeeAge: "22",
+    employeeMobileNumber: "+91 8349317060",
+    employeeAddress: "Ujjain M.P.",
+    employeeGender: "Male",
+    employeeIsMerried: "No"
+}
+for (let a in obj){
+    console.log(a + " : " +obj[a])
+}
