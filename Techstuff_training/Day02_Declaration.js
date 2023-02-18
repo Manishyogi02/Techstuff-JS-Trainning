229664609c84f60d6641698f5ca3b1bfea8892bf
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-18 11:49:36 +0530

    Update Day02_Declaration.js

--- Techstuff_training/Day02_Declaration.js
+++ Techstuff_training/Day02_Declaration.js
@@ -35,3 +35,21 @@ function Form() {
     console.log(employeeeMaritalStatus)
 }
 Form()
+
+// 3. Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object.
+const employeeDetails = {
+    "employeeName" : null,
+    "employeeEmail" : null,
+    "employeeAge" : null,
+    "employeeAddress" : null,
+    "employeePhoneNumber" : null,
+    "employeeGender" : null,
+    "employeeMaritialStatus" : null,
+}
+console.log(employeeDetails["employeeName"]),
+console.log(employeeDetails["employeeEmail"]),
+console.log(employeeDetails["employeeAge"]),
+console.log(employeeDetails["employeeAddress"]),
+console.log(employeeDetails["employeePhoneNumber"]),
+console.log(employeeDetails["employeeGender"]),
+console.log(employeeDetails["employeeMaritialStatus"])
