610df5c886cf71351e0585871801eb448f00e535
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-18 11:48:19 +0530

    Update Day02_Declaration.js

--- Techstuff_training/Day02_Declaration.js
+++ Techstuff_training/Day02_Declaration.js
@@ -16,3 +16,22 @@ function Form() {
     console.log(employeeeMaritalStatus)
 }
 Form()
+
+//2. After printing their value assign them as null and print it again.
+let employeeeName = null;
+let employeeeEmail = null;
+let employeeeAge = null;
+let employeeeAddress = null;
+let employeeePhoneNumber = null;
+let employeeeGender = null;
+let employeeeMaritalStatus = null;
+function Form() {
+    console.log(employeeeName)
+    console.log(employeeeEmail)
+    console.log(employeeeAge)
+    console.log(employeeeAddress)
+    console.log(employeeePhoneNumber)
+    console.log(employeeeGender)
+    console.log(employeeeMaritalStatus)
+}
+Form()
