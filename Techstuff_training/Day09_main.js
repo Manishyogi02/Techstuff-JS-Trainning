ee1af94fb24f9169fed08f22e1afdd93af553de9
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-20 19:12:12 +0530

    Update Day09_main.js

--- Techstuff_training/Day09_main.js
+++ Techstuff_training/Day09_main.js
@@ -1,4 +1,6 @@
-// main.js
+/* main.js 
+// This is our main file, in this Js file we call above function and also JSON data
+4th file */
 
 const { getEmployeesWithNameContainingAM } = require('./Day09_JSONfunction');
 const employees = require('./Day09_employees.json');
