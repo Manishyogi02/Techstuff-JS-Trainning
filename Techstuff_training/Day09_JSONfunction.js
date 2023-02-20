995e1e66b228a6e0df5c78dc0c5d14ec2a087cc5
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-20 19:14:57 +0530

    Update Day09_JSONfunction.js

--- Techstuff_training/Day09_JSONfunction.js
+++ Techstuff_training/Day09_JSONfunction.js
@@ -1,4 +1,5 @@
-// 2. Add a function to return list of employees that contains "am" in his name. Add it in separate file and import it to call in another file.
+/* 2. Add a function to return list of employees that contains "am" in his name. Add it in separate file and import it to call in another file.
+3rd file */
 function getEmployeesWithNameContainingAM(employees) {
     return employees.filter(employee => employee.name.includes('am'));
   }
