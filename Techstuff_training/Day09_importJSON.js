4292ba73a138a358fd07285e6ddfecb2f1fe1635
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-20 19:13:44 +0530

    Update Day09_importJSON.js

--- Techstuff_training/Day09_importJSON.js
+++ Techstuff_training/Day09_importJSON.js
@@ -1,5 +1,6 @@
-// 1. Add the list of employees in a json file and import it in below file to use it. Add unique ids for each employee.
-// create this JS file for exportiong JSON file's data
+/* 1. Add the list of employees in a json file and import it in below file to use it. Add unique ids for each employee.
+ create this JS file for exportiong JSON file's data
+2nd file */
 const employees = require('./Day09_employees.json');
 
 console.log(employees);
