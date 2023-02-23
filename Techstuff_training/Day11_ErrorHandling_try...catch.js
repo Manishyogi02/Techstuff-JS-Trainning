f3205bf7947656584eaaebd95e554daaec70bb48
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-23 16:25:46 +0530

    Update Day11_ErrorHandling_try...catch.js

--- Techstuff_training/Day11_ErrorHandling_try...catch.js
+++ Techstuff_training/Day11_ErrorHandling_try...catch.js
@@ -1,3 +1,4 @@
+// 1.Divide the employee age by null and handle the error. Also if the error occurs divide the age by 2 and print it.
 let employeeAge = prompt("Enter Employee Age :- ")
 try{
     let result = employeeAge / null;
