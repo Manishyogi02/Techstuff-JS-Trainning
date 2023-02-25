a3e93fc211e5f1a6c9cf4b2997f3340f7998d29a
Manish Yogi <120096510+Manishyogi02@users.noreply.github.com>
2023-02-25 15:11:54 +0530

    Update Day12_Promises.js

--- Techstuff_training/Day12_Promises.js
+++ Techstuff_training/Day12_Promises.js
@@ -15,3 +15,26 @@ function getEmployees(){
 getEmployees()
 .then(employees =>console.log(employees))
 .catch(error =>console.error(error))
+
+// 2.Create another promisified function that sorts this employee list from above response by name. Chain it to above promise
+// Define a function to sort the employee list by name
+function sortByName(employees) {
+  return new Promise((resolve, reject) => {
+    if (!employees || !Array.isArray(employees)) {
+      reject(new Error('Invalid employee list'));
+    } else {
+      employees.sort((a, b) => (a.name > b.name ? 1 : -1));
+      resolve(employees);
+    }
+  });
+}
+
+// Call the getEmployees function and chain the sortByName function to it
+getEmployees()
+  .then(sortByName)
+  .then((sortedEmployees) => {
+    console.log('Sorted employees:', sortedEmployees);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
